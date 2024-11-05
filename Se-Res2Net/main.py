b8a3588882cd840c0bddf602f0df90526764a313
blasiasia <ziyoung918@ewhain.net>
2024-11-06 05:18:05 +0900

    database01 stereo->mono로 변환해서 evaluation 완료

--- Se-Res2Net/main.py
+++ Se-Res2Net/main.py
@@ -22,7 +22,7 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchcontrib.optim import SWA
 
-from data_utils import (TrainDataset,TestDataset, genSpoof_list, genSpoof_list_InTheWild)
+from data_utils import (TrainDataset,TestDataset, genSpoof_list)
 from eval.calculate_metrics import calculate_minDCF_EER_CLLR, calculate_aDCF_tdcf_tEER
 from utils import create_optimizer, seed_worker, set_seed, str_to_bool
 
@@ -276,38 +276,6 @@ def produce_evaluation_file(
             spk_id, utt_id, _, src, key = trl.strip().split(' ')
             assert fn == utt_id
             fh.write("{} {} {} {}\n".format(spk_id, utt_id, sco, key))
-    print("Scores saved to {}".format(save_path))
-
-def produce_evaluation_file_InTheWild(
-    data_loader: DataLoader,
-    model,
-    device: torch.device,
-    save_path: str,
-    trial_path: str) -> None:
-    """Perform evaluation and save the score to a file"""
-    model.eval()
-    with open(trial_path, "r") as f_trl:
-        trial_lines = f_trl.readlines()
-    fname_list = []
-    score_list = []
-    for batch_x, utt_id in tqdm(data_loader):
-        batch_x = batch_x.to(device)
-        with torch.no_grad():
-            #_, batch_out = model(batch_x)
-            batch_out = model(batch_x)
-            batch_score = (batch_out[:, 1]).data.cpu().numpy().ravel()
-        # add outputs
-        fname_list.extend(utt_id)
-        score_list.extend(batch_score.tolist())
-
-    #assert len(trial_lines) == len(fname_list) == len(score_list)
-    with open(save_path, "w") as fh:
-        for fn, sco, trl in zip(fname_list, score_list, trial_lines):
-            spk_id, utt_id, _, src, key = trl.strip().split(' ')
-            _, _, _, utt_id = utt_id.strip().split("_")
-            assert fn == utt_id
-            fh.write("{} {} {} {}\n".format(spk_id, utt_id, sco, key))
-
     print("Scores saved to {}".format(save_path))
 
 def train_epoch(
