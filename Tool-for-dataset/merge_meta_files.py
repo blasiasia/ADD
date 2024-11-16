a891fc5ab5cde099ed423d0b24f29cc9abcc71a5
lowell <ziyoung918@ewhain.net>
2024-11-16 15:50:45 +0900

    data loader 및 metadata 파일명 수정 중

--- Tool-for-dataset/merge_meta_files.py
+++ Tool-for-dataset/merge_meta_files.py
@@ -14,7 +14,7 @@ def merge_meta_files(meta_dir, output_file):
                 print(f"Added {file_name} to {output_file}")
 
 if __name__ == "__main__":
-    meta_dir = r'/Volumes/System/MeloTTS/wikipedia/metadata'  # 메타데이터 파일들이 들어 있는 폴더
-    output_file = r'/Volumes/System/MeloTTS/wikipedia/old_melotts_wiki_meta.txt'  # 결과를 저장할 파일
+    meta_dir = r'/Volumes/System/metadata'  # 메타데이터 파일들이 들어 있는 폴더
+    output_file = r'/Volumes/System/eval_meta.txt'  # 결과를 저장할 파일
 
     merge_meta_files(meta_dir, output_file)
